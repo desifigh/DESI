4f00bc0cc0b024082e50c17ae403f5dd463eb5f6
Akshu <109706031+akshux0@users.noreply.github.com>
2023-07-24 14:06:29 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -173,7 +173,7 @@ async def start_comm(client, message: Message, _):
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/DevilsHeavenMF"
+                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/P_B_C_H_A_T"
                         ),
                     ],
                 ]
